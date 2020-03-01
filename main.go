7d35901d318fc43df9f05816255ce5137a0d33ff
Guo Y.K <hi@guoyk.net>
2020-03-01 13:16:48 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/go-redis/redis"
@@ -12,22 +12,147 @@ import (
 	"strings"
 )
 
+type KeyScanner func(key string) (err error)
+
 var (
-	optHost      string
-	optPort      int
-	optDB        int
-	optPassword  string
-	optScanBatch int64
-	optLimit     int64
-	optPrefixes  string
+	ErrStopScanning = errors.New("stop scanning")
+)
+
+type Summary struct {
+	Count       int64
+	Type        string
+	SamplesSize int64
+	Samples     int64
+	MultiType   bool
+}
+
+var (
+	optHost        string
+	optPort        int
+	optDB          int
+	optPassword    string
+	optPrefixes    string
+	optScanBatch   int64
+	optMaxScans    int64
+	optMaxUnknowns int64
+	optMaxSamples  int64
 )
 
 var (
-	knownPrefixes []string
+	rc *redis.Client
 
-	data   = map[string]int64{}
-	others int64
+	knows        = map[string]*Summary{}
+	unknowns     []string
+	moreUnknowns bool
+
+	countTotal   int64
+	countKnown   int64
+	countUnknown int64
 )
+
+func loadKnows() (err error) {
+	var buf []byte
+	if buf, err = ioutil.ReadFile(optPrefixes); err != nil {
+		return
+	}
+
+	deDup := map[string]bool{}
+
+	lines := strings.Split(string(buf), "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if len(line) == 0 {
+			continue
+		}
+		deDup[line] = true
+	}
+
+	for k := range deDup {
+		knows[k] = &Summary{}
+	}
+
+	return
+}
+
+func createKnownPrefixes() (ret []string) {
+	for key := range knows {
+		ret = append(ret, key)
+	}
+	return
+}
+
+func sortKnownPrefixesByLength(knownPrefixes []string) {
+	sort.Slice(knownPrefixes, func(i, j int) bool {
+		return len(knownPrefixes[i]) > len(knownPrefixes[j])
+	})
+	return
+}
+
+func sortKnownPrefixesByCount(knownPrefixes []string) {
+	sort.Slice(knownPrefixes, func(i, j int) bool {
+		return knows[knownPrefixes[i]].Count < knows[knownPrefixes[j]].Count
+	})
+	return
+}
+
+func loadRedis() (err error) {
+	rc = redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", optHost, optPort),
+		DB:       optDB,
+		Password: optPassword,
+	})
+
+	if err = rc.Ping().Err(); err != nil {
+		_ = rc.Close()
+		rc = nil
+		return
+	}
+	return
+}
+
+func scanKeys(scanner KeyScanner) (err error) {
+	var cursor uint64
+	for {
+		// scan
+		var keys []string
+		if keys, cursor, err = rc.Scan(cursor, "", optScanBatch).Result(); err != nil {
+			return
+		}
+		// send to scanner
+		for _, key := range keys {
+			if err = scanner(key); err != nil {
+				break
+			}
+		}
+		// end of scan
+		if cursor == 0 {
+			break
+		}
+	}
+	return
+}
+
+func inspectKey(key string) (typ string, size int64, err error) {
+	if typ, err = rc.Type(key).Result(); err != nil {
+		return
+	}
+	switch typ {
+	case "string":
+		size, err = rc.StrLen(key).Result()
+	case "set":
+		size, err = rc.SCard(key).Result()
+	case "list":
+		size, err = rc.LLen(key).Result()
+	case "zset":
+		size, err = rc.ZCard(key).Result()
+	case "hash":
+		size, err = rc.HLen(key).Result()
+	default:
+		size = 0
+	}
+
+	return
+}
 
 func exit(err *error) {
 	if *err != nil {
@@ -40,96 +165,120 @@ func main() {
 	var err error
 	defer exit(&err)
 
-	flag.StringVar(&optHost, "host", "127.0.0.1", "redis host")
-	flag.IntVar(&optPort, "port", 6379, "redis port")
-	flag.IntVar(&optDB, "db", 0, "redis database")
-	flag.StringVar(&optPassword, "password", "", "redis password")
-	flag.Int64Var(&optScanBatch, "scan-batch", 1000, "batch size of SCAN command")
-	flag.Int64Var(&optLimit, "limit", 0, "limit of total keys scanned")
-	flag.StringVar(&optPrefixes, "prefixes", "prefixes.txt", "known prefixes file")
+	flag.StringVar(&optHost, "host", "127.0.0.1", "redis 主机")
+	flag.IntVar(&optPort, "port", 6379, "redis 端口")
+	flag.IntVar(&optDB, "db", 0, "redis 数据库")
+	flag.StringVar(&optPassword, "password", "", "redis 密码")
+	flag.StringVar(&optPrefixes, "prefixes", "prefixes.txt", "前缀文件，每一行为一条前缀")
+	flag.Int64Var(&optScanBatch, "scan-batch", 1000, "单次 SCAN 的数量")
+	flag.Int64Var(&optMaxScans, "max-scans", 0, "最大扫描多少个键, 0 代表无限制，扫描整个库, 默认为 0")
+	flag.Int64Var(&optMaxUnknowns, "max-unknowns", 200, "最多记录多少条未匹配键")
+	flag.Int64Var(&optMaxSamples, "max-samples", 300, "对同一个前缀，最多进行多少次采样，判定平均长度")
 	flag.Parse()
 
-	// load known prefixes
-	var content []byte
-	if content, err = ioutil.ReadFile(optPrefixes); err != nil {
+	if err = loadKnows(); err != nil {
 		return
 	}
 
-	lines := bytes.Split(content, []byte{'\n'})
-	for _, line := range lines {
-		lineStr := strings.TrimSpace(string(line))
-		if len(lineStr) > 0 {
-			knownPrefixes = append(knownPrefixes, lineStr)
-		}
-	}
-
-	sort.Slice(knownPrefixes, func(i, j int) bool {
-		return len(knownPrefixes[i]) > len(knownPrefixes[j])
-	})
-
-	// redis client
-	r := redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", optHost, optPort),
-		DB:       optDB,
-		Password: optPassword,
-	})
-	defer r.Close()
-
-	if err = r.Ping().Err(); err != nil {
+	if err = loadRedis(); err != nil {
 		return
 	}
 
-	// scan
-	var cursor uint64
-	var total int64
-	for {
-		// scan keys
-		var keys []string
-		if keys, cursor, err = r.Scan(cursor, "", optScanBatch).Result(); err != nil {
+	knownPrefixes := createKnownPrefixes()
+	sortKnownPrefixesByLength(knownPrefixes)
+
+	if err = scanKeys(func(key string) (err error) {
+		// max scans
+		if optMaxScans > 0 && countTotal > optMaxScans {
+			err = ErrStopScanning
 			return
 		}
-		// analysis key
-	outerLoop:
-		for _, key := range keys {
-			for _, pfx := range knownPrefixes {
-				if strings.HasPrefix(key, pfx) {
-					data[pfx]++
-					continue outerLoop
+
+		// total counter
+		countTotal++
+
+		// known
+		for _, pfx := range knownPrefixes {
+			if strings.HasPrefix(key, pfx) {
+				// known counter
+				countKnown++
+				// update summary
+				summary := knows[pfx]
+				// count
+				summary.Count++
+				// sample
+				if summary.Samples < optMaxSamples {
+					var typ string
+					var size int64
+					if typ, size, err = inspectKey(key); err != nil {
+						return
+					}
+					// type
+					if summary.Type == "" {
+						summary.Type = typ
+					} else if summary.Type != typ {
+						summary.MultiType = true
+					}
+					// size
+					summary.SamplesSize += size
+					summary.Samples++
 				}
-			}
-			// not found
-			others++
-			if others < 100 {
-				log.Println("Not Matched:", key)
+				return
 			}
 		}
-		// total count
-		total += int64(len(keys))
-		// limit check
-		if optLimit > 0 && total >= optLimit {
-			log.Println("limit reached")
-			break
+
+		// unknown
+		countUnknown++
+		if int64(len(unknowns)) < optMaxUnknowns {
+			unknowns = append(unknowns, key)
+		} else {
+			moreUnknowns = true
 		}
-		// end of scan
-		if cursor == 0 {
-			break
+		return
+	}); err != nil {
+		if err == ErrStopScanning {
+			log.Println("由于数量限制，扫描提前结束扫描")
+			err = nil
+		} else {
+			return
 		}
 	}
 
 	// output
-	log.Println("------------------------------")
-
-	sort.Slice(knownPrefixes, func(i, j int) bool {
-		return data[knownPrefixes[i]] < data[knownPrefixes[j]]
-	})
-
-	var known int64
-	for _, pfx := range knownPrefixes {
-		known += data[pfx]
-		log.Printf("% 12d: %s", data[pfx], pfx)
+	log.Println("注意: 键值大小使用 STRLEN, ZCARD, SCARD, LLEN, HLEN 等计算，不等于内存占用空间")
+	log.Println("注意: 基于前缀的键值大小汇总基于采样与平均值，并不精确")
+	log.Println("注意: 标注为（混合类型）的前缀表明同一个前缀的键值有不同类型")
+	log.Println("------------- 未匹配键 -----------------")
+	for _, key := range unknowns {
+		log.Printf("键名: %s", key)
+		var typ string
+		var size int64
+		if typ, size, err = inspectKey(key); err != nil {
+			return
+		}
+		log.Printf("键值大小: % 12d, 类型: %s", size, typ)
+		log.Println("------------------------------------")
 	}
+	if moreUnknowns {
+		log.Println("更多未匹配键已被省略")
+	}
+	log.Println("------------- 已匹配键 -----------------")
+	sortKnownPrefixesByCount(knownPrefixes)
+	for _, pfx := range knownPrefixes {
+		summary := knows[pfx]
+		var extra string
+		if summary.MultiType {
+			extra = "(混合类型)"
+		}
+		eas := int64(float64(summary.SamplesSize) / float64(summary.Samples))
+		ets := eas * summary.Count
+		log.Printf("前缀: %s", pfx)
+		log.Printf("数量: % 12d, 估计平均键值大小: % 12d, 估计总键值大小: % 12d, 类型: %s%s", summary.Count, eas, ets, summary.Type, extra)
+		log.Println("------------------------------------")
+	}
+	log.Println("------------- 总计 -----------------")
+	log.Printf("总数量：%d", countTotal)
+	log.Printf("未匹配键数量：%d", countUnknown)
+	log.Printf("已匹配键数量：%d", countKnown)
 
-	log.Printf("Total: %d", total)
-	log.Printf("Known: %d", known)
-	log.Printf("Others: %d", others)
 }
